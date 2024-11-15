741378d0b7418d71c7f705e8341c7fb794a31962
Pavel Sviderski <me@psviderski.name>
2024-11-15 21:35:33 +1000

    create containers table in Corrosion store

--- internal/machine/store/schema.sql
+++ internal/machine/store/schema.sql
@@ -12,4 +12,15 @@ CREATE TABLE machines
     info TEXT NOT NULL DEFAULT '{}' CHECK (json_valid(info))
 );
 
+CREATE TABLE containers
+(
+    id           TEXT NOT NULL PRIMARY KEY,
+    container    TEXT NOT NULL DEFAULT '{}' CHECK (json_valid(container)),
+    machine_id   TEXT NOT NULL DEFAULT '',
+    service_id   TEXT AS (json_extract(container, '$.Labels."uncloud.service.id"')),
+    service_name TEXT AS (json_extract(container, '$.Labels."uncloud.service.name"'))
+);
+
 CREATE INDEX idx_machines_name ON machines (name);
+CREATE INDEX idx_containers_service_id ON containers (service_id);
+CREATE INDEX idx_containers_service_name ON containers (service_name);
