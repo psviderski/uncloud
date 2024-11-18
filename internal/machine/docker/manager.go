bf10b21da50f17f6dcfb4d3214a6872440280394
Pavel Sviderski <me@psviderski.name>
2024-11-18 20:07:51 +1000

    minor comment

--- internal/machine/docker/manager.go
+++ internal/machine/docker/manager.go
@@ -21,7 +21,9 @@ const (
 	// SyncInterval defines a regular interval to sync containers to the cluster store.
 	SyncInterval = 30 * time.Second
 
-	// SyncStatusSynced indicates that a container record is synchronised with the Docker state.
+	// SyncStatusSynced indicates that a container record is synchronised with the Docker daemon. The record may
+	// become outdated even when the status is "synced" if the machine crashes or a network partition occurs.
+	// The cluster membership state of the machine should also be checked to determine if the record can be trusted.
 	SyncStatusSynced = "synced"
 	// SyncStatusOutdated indicates that a container record may be outdated, for example, due to being unable
 	// to retrieve the container's state from the Docker daemon or when the machine is being stopped or restarted.
