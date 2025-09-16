3db07ca2d44407cc3fc9d3b1366a16101e3b06f2
Pasha Sviderski <me@psviderski.name>
2025-09-16 10:17:44 +1000

    feat: allow setting config and connection with UNCLOUD_CONFIG and UNCLOUD_CONNECT env vars

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -1,7 +1,11 @@
 package cli
 
 import (
+	"log"
+	"os"
 	"strings"
+
+	"github.com/spf13/cobra"
 )
 
 // ExpandCommaSeparatedValues takes a slice of strings and expands any comma-separated values into individual elements.
@@ -20,4 +24,13 @@ func ExpandCommaSeparatedValues(values []string) []string {
 	}
 
 	return expanded
+}
+
+// BindEnvToFlag assigns the value of an environment variable to the given command flag if the flag has not been set.
+func BindEnvToFlag(cmd *cobra.Command, flagName, envVar string) {
+	if value := os.Getenv(envVar); value != "" && !cmd.Flags().Changed(flagName) {
+		if err := cmd.Flags().Set(flagName, value); err != nil {
+			log.Fatalf("Failed to bind environment variable '%s' to flag '%s': %v", envVar, flagName, err)
+		}
+	}
 }
