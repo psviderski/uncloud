30ccf4f083895949e8e5d95174fdbcd3d11a9d77
Anton Ovchinnikov <anton@tonyo.info>
2025-11-05 23:40:50 +0100

    Improve example

--- cmd/uncloud/service/exec.go
+++ cmd/uncloud/service/exec.go
@@ -41,7 +41,7 @@ If the service has multiple replicas, the command will be executed in a random c
   uc exec --container d792ea7347e5 web-service ls -la
 
   # Pipe input to a command inside the service container
-  cat /var/log/app.log | uc exec -T web-service grep "ERROR"
+  cat backup.sql | uc exec -T db-service psql -U postgres mydb
 
   # Run a task in the background (detached mode)
   uc exec -d web-service /scripts/cleanup.sh`,
