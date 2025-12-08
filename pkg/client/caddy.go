4b4b721d4935c4a9b135164469e69f722c319e08
Frederik Zorn <fz@zorrn.net>
2025-12-08 10:21:30 +0100

    feat(uc/caddy): expose 443/udp port for HTTP3 (#204)
    * feat(caddy): expose 443/udp port for HTTP3
    
    * expose udp port 443 for caddy to support new HTTP3
    
    * chore: Add comment for HTTP/3 (QUIC) port configuration
    
    Added a comment to clarify the purpose of the UDP port configuration.
    
    ---------
    
    Co-authored-by: Anton Ovchinnikov <anton@tonyo.info>

--- pkg/client/caddy.go
+++ pkg/client/caddy.go
@@ -71,6 +71,13 @@ func (cli *Client) NewCaddyDeployment(image, config string, placement api.Placem
 				Protocol:      api.ProtocolTCP,
 				Mode:          api.PortModeHost,
 			},
+			// Needed for HTTP/3 (QUIC)
+			{
+				PublishedPort: 443,
+				ContainerPort: 443,
+				Protocol:      api.ProtocolUDP,
+				Mode:          api.PortModeHost,
+			},
 		},
 		Volumes: []api.VolumeSpec{
 			{
