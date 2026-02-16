a7311d797a39207233ec87d6a9596edf1a9042e5
gm0stache <gm0stache@noreply.codeberg.org>
2026-02-16 16:20:04 +0100

    test: refactor

--- pkg/client/dns_internal_test.go
+++ pkg/client/dns_internal_test.go
@@ -3,27 +3,28 @@ package client
 import (
 	"net/netip"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetVerifyURL(t *testing.T) {
 	tests := map[string]struct {
-		ip   netip.Addr
-		want string
+		given netip.Addr
+		want  string
 	}{
 		"IPv4": {
-			ip:   netip.MustParseAddr("93.184.216.34"),
-			want: "http://93.184.216.34:/.uncloud-verify",
+			given: netip.MustParseAddr("93.184.216.34"),
+			want:  "http://93.184.216.34:/.uncloud-verify",
 		},
 		"IPv6": {
-			ip:   netip.MustParseAddr("2001:db8::1"),
-			want: "http://[2001:db8::1]:/.uncloud-verify",
+			given: netip.MustParseAddr("2001:db8::1"),
+			want:  "http://[2001:db8::1]:/.uncloud-verify",
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			if got := getVerifyURL(tt.ip); got != tt.want {
-				t.Errorf("getVerifyURL() = %q, want %q", got, tt.want)
-			}
+			got := getVerifyURL(tt.given)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
