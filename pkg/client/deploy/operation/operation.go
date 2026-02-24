93d7f29d316c48d9844b99f7651db2759dd6390c
Pasha Sviderski <me@psviderski.name>
2026-02-24 13:17:59 +1000

    chore: remove TODO about encapsulating Client in deploy operations

--- pkg/client/deploy/operation/operation.go
+++ pkg/client/deploy/operation/operation.go
@@ -10,8 +10,6 @@ import (
 // Operations can be composed to form complex deployment strategies.
 type Operation interface {
 	// Execute performs the operation using the provided client.
-	// TODO: Encapsulate the client in the operation as otherwise it gives an impression that different clients
-	//  can be provided. But in reality, the operation is tightly coupled with the client that was used to create it.
 	Execute(ctx context.Context, cli Client) error
 	// Format returns a human-readable representation of the operation.
 	// TODO: get rid of the resolver and assign the required names for formatting in the operation itself.
@@ -19,16 +17,14 @@ type Operation interface {
 	String() string
 }
 
-// NameResolver resolves machine and container IDs to their names.
-type NameResolver interface {
-	MachineName(machineID string) string
-	ContainerName(containerID string) string
-}
-
-// TODO: pass api.ServiceContainer to operations to simplify operation formatting in the plan.
-
 // Client defines the interface required to execute deployment operations.
 type Client interface {
 	api.ContainerClient
 	api.VolumeClient
+}
+
+// NameResolver resolves machine and container IDs to their names.
+type NameResolver interface {
+	MachineName(machineID string) string
+	ContainerName(containerID string) string
 }
