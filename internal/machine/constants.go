fbbd9022125428ca4a1772c24351c3b47c3f56ba
Tang Vu <145498528+tang-vu@users.noreply.github.com>
2026-03-28 23:29:41 +0700

    refactor: update corrosion version to v0.2.3
    Update the Corrosion version to v0.2.3 to use the binary built with an older GLIBC (ubuntu-20.04), which adds support for Amazon Linux 2023 (GLIBC 2.34).
    
    Affected files: constants.go
    
    Signed-off-by: Tang Vu <145498528+tang-vu@users.noreply.github.com>

---
+++ internal/machine/constants.go
@@ -0,0 +1,9 @@
+package machine
+
+const (
+	// DefaultDataDir is the default directory for storing persistent machine state.
+	DefaultDataDir = "/var/lib/uncloud"
+
+	// CorrosionVersion is the version of Corrosion to use.
+	CorrosionVersion = "v0.2.3"
+)
