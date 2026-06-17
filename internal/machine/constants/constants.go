d36ff674099df2c806722b327c04dd63931005f0
Pasha Sviderski <me@psviderski.name>
2026-06-17 14:37:09 +1000

    unregistry: change the listening port on each machine 5000 -> 51500

--- internal/machine/constants/constants.go
+++ internal/machine/constants/constants.go
@@ -4,5 +4,5 @@ const (
 	// MachineAPIPort is the port for the Machine API service on the management WireGuard network.
 	MachineAPIPort = 51000
 	// UnregistryPort is the port for the embedded container registry listening on the machine IP.
-	UnregistryPort = 5000
+	UnregistryPort = 51500
 )
